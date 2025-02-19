9f73ddb468bc7849ec264b8c4f85ec740d2cfe27
Michael Marwa <marwakmichael@gmail.com>
2025-02-19 12:23:34 +0200

    reset the entire project

--- FLH-decor-storefront/src/modules/layout/templates/nav/mobile-nav.tsx
+++
@@ -1,9 +0,0 @@
-import NavItems from './nav-items';
-
-export default function MobileNav() {
-  return (
-    <div className="flex flex-col space-y-4 p-4">
-      <NavItems mobileView />
-    </div>
-  );
-}
--- FLH-decor-storefront/src/types/heroicons.d.ts
+++
@@ -1,8 +0,0 @@
-// src/types/heroicons.d.ts
-declare module '@heroicons/react/solid' {
-    import { FC, SVGAttributes } from 'react';
-    
-    export const MenuIcon: FC<SVGAttributes<SVGElement>>;
-    export const XIcon: FC<SVGAttributes<SVGElement>>;
-    // Add other icons as needed
-  }
